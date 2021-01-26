71eef7ef2f74489e4a38fbd5687224ea1122114f
Alexey Zuev <73071789+43art@users.noreply.github.com>
2021-01-26 23:32:11 +0300

    Merge pull request #4

---
+++ src/mire/player.clj
@@ -0,0 +1,12 @@
+(ns mire.player)
+
+(def ^:dynamic *current-room*)
+(def ^:dynamic *inventory*)
+(def ^:dynamic *name*)
+(def ^:dynamic *healthpoints* 5)
+
+(def prompt "> ")
+(def streams (ref {}))
+
+(defn carrying? [thing]
+  (some #{(keyword thing)} @*inventory*))
